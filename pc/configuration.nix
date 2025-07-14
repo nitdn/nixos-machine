b6742a5cb85022ddfb9cc1c126bec3389c6a5aab
Nitesh Kumar Debnath <30214255+nitdn@users.noreply.github.com>
2025-07-14 13:27:27 +0530

    Adds steam

--- pc/configuration.nix
+++ pc/configuration.nix
@@ -127,6 +127,12 @@ in
   # Install firefox.
   programs.firefox.enable = true;
   programs.fish.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   programs.gamemode.enable = true;
   programs.bash = {
     interactiveShellInit = ''
