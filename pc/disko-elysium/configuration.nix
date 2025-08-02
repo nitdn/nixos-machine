18ffa4e9e20f4ae3e32260b96b5b7acf953e7c64
Nitesh Kumar Debnath <30214255+nitdn@users.noreply.github.com>
2025-08-02 15:18:39 +0530

    Adds input group

--- pc/disko-elysium/configuration.nix
+++ pc/disko-elysium/configuration.nix
@@ -72,7 +72,10 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ssmvabaa = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "wheel"
+      "input"
+    ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
