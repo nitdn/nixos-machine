8823b49d78d39e67ef9e108e4faf6c2f7028924d
Nitesh Debnath <30214255+nitdn@users.noreply.github.com>
2025-11-02 09:32:07 +0000

    Try to fix the broken garnix #1

--- modules/perSystem/overlays.nix
+++ modules/perSystem/overlays.nix
@@ -15,8 +15,8 @@
       overlayAttrs = {
         # inherit (config.legacyPackages) qt6Packages;
       };
-      legacyPackages.qt6Packages = pkgs.qt6Packages // {
-        inherit (inputs'.stablepkgs.legacyPackages.qt6Packages) fcitx5-qt;
-      };
+      # legacyPackages.qt6Packages = pkgs.qt6Packages // {
+      #   inherit (inputs'.stablepkgs.legacyPackages.qt6Packages) fcitx5-qt;
+      # };
     };
 }
