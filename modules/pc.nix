91407facbeac732fe562acbd63a13d52672b0247
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2025-12-13 09:35:19 +0530

    feat(pc): zswap slightly faster swap

--- modules/pc.nix
+++ modules/pc.nix
@@ -40,6 +40,13 @@
       # Bootloader.
       boot.loader.limine.enable = true;
       boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+      boot.initrd.systemd.enable = true;
+      boot.kernelParams = [
+        "zswap.enabled=1" # enables zswap
+        "zswap.compressor=lz4" # compression algorithm
+        "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+        "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+      ];
       boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
       services.fwupd.enable = true;
       networking = {
