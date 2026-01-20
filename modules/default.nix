b6031900c5818ee3d5984f61d1c64bcd14a93a9b
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-01-20 14:23:47 +0530

    chore(secrets): rotate lost keys

--- modules/default.nix
+++ modules/default.nix
@@ -44,8 +44,6 @@ in
       # You can avoid this by adding a string to the full path instead, i.e.
       # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
       sops.defaultSopsFile = ../secrets/core.yaml;
-      # This will automatically import SSH keys as age keys
-      sops.age.sshKeyPaths = [ "/home/${username}/.ssh/id_ed25519" ];
       # This is using an age key that is expected to already be in the filesystem
       sops.age.keyFile = "/var/lib/sops-nix/key.txt";
       # This will generate a new key if the key specified above does not exist
