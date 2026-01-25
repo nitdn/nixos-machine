42400634742e6a0e2adee96c8049f3de3c6ec2f0
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-01-25 09:05:28 +0530

    fix(pc): syncthing will cause me severe brain damage

--- modules/passwords.nix
+++ modules/passwords.nix
@@ -1,22 +1,13 @@
 # SPDX-FileCopyrightText: 2026 Nitesh Kumar Debnath <nitkdnath@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-{ config, ... }:
-let
-  inherit (config.meta) username;
-in
 {
   flake.modules.nixos.pc =
-    { pkgs, config, ... }:
+    { pkgs, ... }:
     {
-      services.syncthing = {
-        enable = true;
-        user = username;
-        group = username;
-        dataDir = "/home/${username}/";
-        configDir = config.services.syncthing.dataDir + "/.local/state/syncthing";
-      };
       environment.systemPackages = [ pkgs.keepassxc ];
     };
+  flake.modules.homeManager.pc = {
+    services.syncthing.enable = true;
+  };
 }
