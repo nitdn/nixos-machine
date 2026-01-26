4b50115ba746d6e6ba847c0d5c5d0fc7372b526a
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-01-26 21:10:00 +0530

    refactor(pc): kill ifd for good
    i dont believe in giving my opps a good time

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,7 +4,6 @@
 
 {
   flake.modules.nixos.pc =
-    { pkgs, ... }:
     let
       tls_auth_name = "dns.adguard-dns.com";
     in
@@ -13,7 +12,19 @@
       networking.dhcpcd.wait = "background";
       services.stubby = {
         enable = true;
-        settings = pkgs.stubby.passthru.settingsExample // {
+        settings = {
+          dns_transport_list = [ "GETDNS_TRANSPORT_TLS" ];
+          edns_client_subnet_private = 1;
+          idle_timeout = 10000;
+          listen_addresses = [
+            "127.0.0.1"
+            "0::1"
+          ];
+          log_level = "GETDNS_LOG_NOTICE";
+          resolution_type = "GETDNS_RESOLUTION_STUB";
+          round_robin_upstreams = 1;
+          tls_authentication = "GETDNS_AUTHENTICATION_REQUIRED";
+          tls_query_padding_blocksize = 128;
           upstream_recursive_servers = [
             {
               address_data = "94.140.14.14";
