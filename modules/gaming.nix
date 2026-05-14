27c4312021dd76bbe697f01cd9d50c0a2b3ab229
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-05-14 17:51:47 +0530

    feat(pc): ge-proton

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -27,6 +27,9 @@ in
           remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
           dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         };
+        programs.steam.extraCompatPackages = [
+          pkgs.proton-ge-bin
+        ];
         programs.steam.package = lib.mkDefault (
           pkgs.steam.override {
             extraPkgs =
