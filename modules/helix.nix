ab66b40e0235721151590705d8aff4aec65cc640
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-06-15 18:47:18 +0530

    helix: change theme to more orange

--- modules/helix.nix
+++ modules/helix.nix
@@ -18,7 +18,7 @@ let
     {
       imports = [ wlib.wrapperModules.helix ];
       settings = {
-        theme = lib.mkDefault "rose_pine";
+        theme = lib.mkDefault "ashen";
         editor = {
           line-number = "relative";
           # end-of-line-diagnostics = "hint";
@@ -110,7 +110,7 @@ let
     };
   helix-light = {
     imports = [ config.flake.wrapperModules.helix-pc ];
-    settings.theme = "rose_pine_dawn";
+    settings.theme = "ingrid";
   };
 in
 {
